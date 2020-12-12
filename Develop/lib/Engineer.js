ee7e7bc87139cf86671487b08cd0934d3218dc31
Kailee Smith <kaileesmith1501@gmail.com>
2020-12-11 20:59:51 -0800

    added to Engineer class

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -6,4 +6,12 @@ class Engineer extends Employee {
         super(name, id, email);
         this.github = github;
     }
+    getRole(){
+        return "Engineer";
+    }
+    getGithub(){
+        return this.github;
+    }
 }
+
+module.exports = Engineer;

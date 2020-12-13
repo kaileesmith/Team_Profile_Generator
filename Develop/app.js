459ed1d1cad557c8b073f708231050e035ee75a5
Kailee Smith <kaileesmith1501@gmail.com>
2020-12-13 10:31:20 -0800

    changing format to be more concise for questions

--- Develop/app.js
+++ Develop/app.js
@@ -37,21 +37,47 @@ const render = require("./lib/htmlRenderer");
 let employees = [];
 let id= 0;
 
-let questions = [
-    {
-        type:"input",
-        name: "name",
-        message: "What is the employee's name?"
-    },
-    {
-        type:"list",
-        name: "position",
-        message: "What is the employee's position?",
-        choices:["Manager", "Engineer", "Intern"]
+function addEmployee (){
+    inquirer.prompt([
+        {
+            type:"input",
+            name: "name",
+            message: "What is the employee's name?"
+        },
+        {
+            type:"input",
+            name: "email",
+            message: "What is the employee's email?"
+        },
+        {
+            type:"list",
+            name: "position",
+            message: "What is the employee's position?",
+            choices:["Manager", "Engineer", "Intern"],
+    
+        },
 
-    },
+    ])
 
-]
+
+
+}
+
+// let questions = [
+//     {
+//         type:"input",
+//         name: "name",
+//         message: "What is the employee's name?"
+//     },
+//     {
+//         type:"list",
+//         name: "position",
+//         message: "What is the employee's position?",
+//         choices:["Manager", "Engineer", "Intern"]
+
+//     },
+
+// ]
 // var for each level with question object for final question
 let manager = {
         type:"imput",
